f34aae5ddf36c39c3361692f15f45a85137db7f4
Christopher Ruiz <christopher.ruiz@ollintem.com.mx>
2026-09-14 00:26:59 -0600

    fix: routes fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FarmaciasController;
 use Illuminate\Support\Facades\Route;
 
 // Rutas de autenticación
@@ -10,9 +9,6 @@ Route::post('/', [AuthController::class, 'login'])->name('login');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
-
-    // CRUD de farmacias
-    Route::resource('farmacias', FarmaciasController::class);
 });
 
 require __DIR__.'/settings.php';
